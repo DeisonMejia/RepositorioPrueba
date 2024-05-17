f490e36d67b7b6f01e76b07027d6e526763d3e45
DeisonMejia <146489524+DeisonMejia@users.noreply.github.com>
2024-05-17 08:45:16 -0500

    Add files via upload

--- Repositorio/prueba.py
+++ Repositorio/prueba.py
@@ -13,24 +13,4 @@ def division(a, b):
     else:
         return "Error: No se puede dividir por cero"
 
-# Función para la selección de operación
-def calcular(operacion, a, b):
-    if operacion == '+':
-        return suma(a, b)
-    elif operacion == '-':
-        return resta(a, b)
-    elif operacion == '*':
-        return multiplicacion(a, b)
-    elif operacion == '/':
-        return division(a, b)
-    else:
-        return "Operación no válida"
 
-# Ejemplo de uso
-print("Operaciones disponibles: +, -, *, /")
-operacion = input("Ingrese la operación que desea realizar: ")
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-
-resultado = calcular(operacion, num1, num2)
-print("El resultado es:", resultado)
